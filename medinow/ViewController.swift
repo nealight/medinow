e8c4318592b368918a98466e4879ace79a694ab4
lighttttt <yxu70832@usc.edu>
2023-04-21 17:41:48 -0400

    Initial Commit

---
+++ medinow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  medinow
+//
+//  Created by Yi Xu on 4/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
